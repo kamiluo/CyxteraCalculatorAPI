264d906c350cb71e5b55711b0842417760bc893a
CamiloPC <CamiloPC@DESKTOP-LR0670C>
2019-05-21 21:43:02 -0500

    Se agregan tests para probar.

--- CyxteraCalculatorAPI/src/test/java/com/caforerof/calculator/CyxteraCalculatorApiApplicationTests.java
+++ CyxteraCalculatorAPI/src/test/java/com/caforerof/calculator/CyxteraCalculatorApiApplicationTests.java
@@ -23,23 +23,68 @@ public class CyxteraCalculatorApiApplicationTests {
 
 	@Test
 	public void getSessionIdServiceSuccessfullResponseTest() throws Exception {
-		this.mockMvc.perform(get("/getSession"))
+		this.mockMvc.perform(get("/calculatorApi/getSessionId"))
         .andDo(print()).andExpect(status().isOk())
         .andExpect(jsonPath("$.sessionId").exists());
 	}
 	
 	@Test
-	public void setOperatorServiceSuccessfullResponseTest() throws Exception {
-		this.mockMvc.perform(get("/addOperator").param("value", "1.0"))
+	public void addOperandServiceSuccessfullResponseTest() throws Exception {
+		double operand = 1.0;
+		this.mockMvc.perform(get("/calculatorApi/addOperand/" + operand))
         .andDo(print()).andExpect(status().isOk())
-        .andExpect(jsonPath("$.operator").value("1.0"));
+        .andExpect(jsonPath("$.operand").value(operand));
 	}
 	
 	@Test
-	public void setOperandServiceSuccessfullResponseTest() throws Exception {
-		this.mockMvc.perform(get("/addOperand").param("value", "+"))
+	public void setOperatorServiceSuccessfullResponseTest() throws Exception {
+		String operator = "suma";
+		this.mockMvc.perform(get("/calculatorApi/setOperator/" + operator))
         .andDo(print()).andExpect(status().isOk())
-        .andExpect(jsonPath("$.operand").value("+"));
+        .andExpect(jsonPath("$.operator").value(operator));
+	}
+	
+	@Test
+	public void getSessionIdServiceFailBdTest() throws Exception {
+		this.mockMvc.perform(get("/calculatorApi/getSessionId"))
+        .andDo(print()).andExpect(status().is5xxServerError())
+        .andExpect(jsonPath("$.sessionId").exists());
+	}
+	
+	@Test
+	public void addOperandServiceFailStringTest() throws Exception {
+		String operand = "stringTest";
+		this.mockMvc.perform(get("/calculatorApi/addOperand/" + operand))
+        .andDo(print()).andExpect(status().is5xxServerError())
+        .andExpect(jsonPath("$.operand").value(operand));
+	}
+	
+	@Test
+	public void setOperatorServiceFailNotStringTest() throws Exception {
+		long operator = 1;
+		this.mockMvc.perform(get("/calculatorApi/setOperator/" + operator))
+        .andDo(print()).andExpect(status().is5xxServerError())
+        .andExpect(jsonPath("$.operator").value(operator));
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void validateAddNumbers() throws Exception{
+		double[] operand = {1.0, 2.0, 3.0};
+		long sessionId = 0;
+		this.mockMvc.perform(get("/calculatorApi/getSessionId"))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.sessionId").exists());
+		
+		this.mockMvc.perform(get("/calculatorApi/addOperand/" + operand[0]))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.operand").value(operand[0]));
+		this.mockMvc.perform(get("/calculatorApi/addOperand/" + operand[1]))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.operand").value(operand[1]));
+		this.mockMvc.perform(get("/calculatorApi/addOperand/" + operand[2]))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.operand").value(operand[2]));
 	}
 
 }
